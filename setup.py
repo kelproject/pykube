33288c032590bb91a3c7e759cc8b567ddab21f43
Brian Rosner <brian@brosner.com>
2015-08-14 09:26:41 -0600

    switched to semantic versioning

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pykube",
-    version="0.1a1",
+    version="0.1.0",
     description="",
     long_description="",
     author="Eldarion, Inc.",
