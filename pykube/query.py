b553c36addfb8056be37d75fb6eac6a7c4a8c8c1
Brian Rosner <brian@brosner.com>
2016-01-30 13:44:10 -0700

    Improved selector querying

--- pykube/query.py
+++ pykube/query.py
@@ -28,7 +28,7 @@ class Query:
         if not hasattr(self, "_query_cache"):
             params = {}
             if self.selector is not everything:
-                params["labelSelector"] = self.selector
+                params["labelSelector"] = as_selector(self.selector)
             query_string = urlencode(params)
             r = self.api.get(
                 url="{}{}".format(self.endpoint, "?{}".format(query_string) if query_string else ""),
@@ -57,3 +57,30 @@ class ObjectManager:
         assert obj is None, "cannot invoke objects on resource object."
         self.api_obj_class = api_obj_class
         return self
+
+
+def as_selector(value):
+    if isinstance(value, str):
+        return value
+    s = []
+    for k, v in value.items():
+        bits = k.split("__")
+        assert len(bits) <= 2, "too many __ in selector"
+        if len(bits) == 1:
+            label = bits[0]
+            op = "eq"
+        else:
+            label = bits[0]
+            op = bits[1]
+        # map operator to selector
+        if op == "eq":
+            s.append("{} = {}".format(label, v))
+        elif op == "neq":
+            s.append("{} != {}".format(label, v))
+        elif op == "in":
+            s.append("{} in ({})".format(label, ",".join(v)))
+        elif op == "notin":
+            s.append("{} notin ({})".format(label, ",".join(v)))
+        else:
+            raise ValueError("{} is not a valid comparsion operator".format(op))
+    return ",".join(s)
