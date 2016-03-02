616f4e5225a88a8cf7fe7cc8e40aa037b3b20df1
Brian Rosner <brian@brosner.com>
2016-03-01 18:10:34 -0700

    Bumped version for next release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst") as fp:
 
 setup(
     name="pykube",
-    version="0.7.3",
+    version="0.8.0",
     description="Python client library for Kubernetes",
     long_description=long_description,
     author="Eldarion, Inc.",
