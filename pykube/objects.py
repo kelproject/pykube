a36c145a689d5a0eb27cf9384c9c0b07df8d2165
Pablo Moreno <pablo.a.moreno@gmail.com>
2016-04-18 12:13:56 +0100

    Fixes output, URL formation and adds further parameters.

--- pykube/objects.py
+++ pykube/objects.py
@@ -176,20 +176,36 @@ class Pod(NamespacedAPIObject):
         condition = next((c for c in cs if c["type"] == "Ready"), None)
         return condition is not None and condition["status"] == "True"
 
-    def get_logs(self, container=None):
-        url = "logs"
+    def get_logs(self, container=None, pretty=None, previous=False,
+                 since_seconds=None, since_time=None, timestamps=False,
+                 tail_lines=None, limit_bytes=None):
+        """Produces the same result as calling kubectl logs pod/<pod-name>"""
+        url = "log"
         params = {}
         if container is not None:
             params["container"] = container
+        if pretty is not None:
+            params["pretty"] = pretty
+        if previous:
+            params["previous"] = "true"
+        if since_seconds is not None and since_time is None:
+            params["sinceSeconds"] = int(since_seconds)
+        elif since_time is not None and since_seconds is None:
+            params["sinceTime"] = since_time
+        if timestamps:
+            params["timestamps"] = "true"
+        if tail_lines is not None:
+            params["tailLines"] = int(tail_lines)
+        if limit_bytes is not None:
+            params["limitBytes"] = int(limit_bytes)
+
         query_string = urlencode(params)
         url += "?{}".format(query_string) if query_string else ""
-        kwargs = {'url': url,
-                  'pods': self.name,
-                  'namespace': self.namespace,
-                  'version': self.version}
+        kwargs = {'url': url, 'pods': self.name,
+                  'namespace': self.namespace, 'version': self.version}
         r = self.api.get(**kwargs)
         r.raise_for_status()
-        return r.json()
+        return r.text
 
 
 class ReplicationController(NamespacedAPIObject, ReplicatedAPIObject):
